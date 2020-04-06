97ec9380224f25a2b00ce0239f41b36e4c5f64c0
Chaos-li <179988788@qq.com>
2020-04-06 13:19:52 +0800

    管理员相关接口

--- src/main/java/com/yihukurama/sysbase/controller/admin/ManagerController.java
+++ src/main/java/com/yihukurama/sysbase/controller/admin/ManagerController.java
@@ -1,6 +1,8 @@
 package com.yihukurama.sysbase.controller.admin;
 
 import com.yihukurama.sysbase.controller.admin.dto.LoginDTO;
+import com.yihukurama.sysbase.controller.admin.dto.ManagerModifyDTO;
+import com.yihukurama.sysbase.controller.admin.dto.ModifyPassWordDTO;
 import com.yihukurama.sysbase.controller.app.dto.LoginDto;
 import com.yihukurama.sysbase.module.admin.Manager;
 import com.yihukurama.tkmybatisplus.framework.web.dto.Request;
@@ -25,9 +27,21 @@ public class ManagerController {
     @Resource
     Manager manager;
 
-    @ApiOperation(value = "登录接口",notes = "用户名密码必传-密码前端加密")
+    @ApiOperation(value = "登录接口",notes = "用户名密码必传-密码后端加密")
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Result login(@RequestBody Request<LoginDTO> request) throws Exception {
         return manager.adminLogin(request);
+    }
+
+    @ApiOperation(value = "修改用户名+手机号",notes = "修改用户名+手机号-带校验")
+    @RequestMapping(value = "/modifyNameAndPhone", method = RequestMethod.POST)
+    public Result modifyNameAndPhone(@RequestBody Request<ManagerModifyDTO> request) throws Exception {
+        return manager.modifyNameAndPhone(request);
+    }
+
+    @ApiOperation(value = "修改密码接口",notes = "修改密码接口-带检验")
+    @RequestMapping(value = "/modifyPassWord", method = RequestMethod.POST)
+    public Result modifyPassWord(@RequestBody Request<ModifyPassWordDTO> request) throws Exception {
+        return manager.modifyPassWord(request);
     }
 }
