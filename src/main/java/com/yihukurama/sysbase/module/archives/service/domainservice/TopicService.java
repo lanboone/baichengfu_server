18629abacf824c0aec18a25a484229fb3318d436
Chaos-li <179988788@qq.com>
2020-04-19 20:56:14 +0800

    话题list接口的返回增加发布人姓名

--- src/main/java/com/yihukurama/sysbase/module/archives/service/domainservice/TopicService.java
+++ src/main/java/com/yihukurama/sysbase/module/archives/service/domainservice/TopicService.java
@@ -1,11 +1,24 @@
 package com.yihukurama.sysbase.module.archives.service.domainservice;
 
+import com.yihukurama.sysbase.mapper.AppuserMapper;
+import com.yihukurama.sysbase.mapper.ManagerMapper;
+import com.yihukurama.sysbase.model.AppuserEntity;
+import com.yihukurama.sysbase.model.ManagerEntity;
 import com.yihukurama.sysbase.model.TopicEntity;
+import com.yihukurama.sysbase.module.archives.domain.Topic;
 import com.yihukurama.tkmybatisplus.app.exception.TipsException;
 import com.yihukurama.tkmybatisplus.app.utils.EmptyUtil;
 import com.yihukurama.tkmybatisplus.framework.service.domainservice.CrudService;
+import com.yihukurama.tkmybatisplus.framework.web.dto.Result;
 import net.bytebuddy.pool.TypePool;
+import net.sf.jsqlparser.statement.select.Top;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 说明： Topic的领域服务
@@ -16,6 +29,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService extends CrudService<TopicEntity>{
 
+    @Autowired
+    ManagerMapper managerMapper;
+    @Autowired
+    AppuserMapper appuserMapper;
     @Override
     public TopicEntity create(TopicEntity topicEntity) throws TipsException {
         if(EmptyUtil.isEmpty(topicEntity.getTopicImage())
@@ -24,5 +41,39 @@ public class TopicService extends CrudService<TopicEntity>{
             throw new TipsException("话题标题、封面图、内容不可为空");
         }
         return super.create(topicEntity);
+    }
+
+    @Override
+    public Result list(TopicEntity designerEntity, Integer page, Integer limit) throws TipsException {
+        Result result = super.list(designerEntity, page, limit);
+        List<TopicEntity> topicEntitiesList = (List<TopicEntity>) result.getData();
+        List<Topic> topics = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(topicEntitiesList)) {
+            topicEntitiesList.forEach(topicEntityFromDB -> {
+                Topic topic = new Topic();
+                BeanUtils.copyProperties(topicEntityFromDB,topic);
+                // 创建者类型（10管理员，20App用户）
+                if ("20".equals(topicEntityFromDB.getCreatorType())){
+                    ManagerEntity managerEntity = new ManagerEntity();
+                    managerEntity.setId(topicEntityFromDB.getCreaterId());
+                    ManagerEntity managerEntityFromDB = managerMapper.selectOne(managerEntity);
+                    if (managerEntityFromDB != null){
+                        topic.setPublisherName(managerEntityFromDB.getSysName());
+                    }
+                }else if ("10".equals(topicEntityFromDB.getCreatorType())){
+                    AppuserEntity appuserEntity = new AppuserEntity();
+                    appuserEntity.setId(topicEntityFromDB.getCreaterId());
+                    AppuserEntity appuserEntityFromDB = appuserMapper.selectOne(appuserEntity);
+                    if (appuserEntityFromDB != null){
+                        topic.setPublisherName(appuserEntityFromDB.getUserName());
+                    }
+                }
+                topics.add(topic);
+            });
+            result.setData(topics);
+            return result;
+        } else {
+            return result;
+        }
     }
 }
