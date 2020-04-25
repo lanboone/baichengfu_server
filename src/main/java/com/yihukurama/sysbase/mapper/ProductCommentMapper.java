9954320d8e538a3e2f3bfb180117d3db13944d26
Administrator <479757405@qq.com>
2020-04-25 17:37:22 +0800

    增加商品评论接口

---
+++ src/main/java/com/yihukurama/sysbase/mapper/ProductCommentMapper.java
@@ -0,0 +1,16 @@
+package com.yihukurama.sysbase.mapper;
+
+import com.yihukurama.sysbase.model.ProductCommentEntity;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 说明： ProductCommentEntity的通用mapper接口
+ * @author: yihukurama
+ * @date: Created in 15:18 2018/4/4
+ * @modified: by yihukurama in 15:18 2018/4/4
+ */
+@Repository
+public interface ProductCommentMapper extends Mapper<ProductCommentEntity> {
+}
+
---
+++ src/main/java/com/yihukurama/sysbase/model/ProductCommentEntity.java
@@ -0,0 +1,102 @@
+package com.yihukurama.sysbase.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yihukurama.tkmybatisplus.framework.domain.tkmapper.entity.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * 说明： 话题评论表
+ * @author: yihukurama
+ * @date: Created in 15:18 2018/4/4
+ * @modified: by yihukurama in 15:18 2018/4/4
+ */
+@ApiModel(value="话题评论表")
+@Table(name="tb_product_comment")
+@Data
+public class ProductCommentEntity extends BaseEntity
+{
+	/**
+	*创建者昵称
+	**/
+	@ApiModelProperty(value="创建者昵称")
+	@Column(name="comment_name")
+	private String commentName;
+	/**
+	*最后修改人id
+	**/
+	@ApiModelProperty(value="最后修改人id")
+	@Column(name="operator_id")
+	private String operatorId;
+	/**
+	*最后修改日期
+	**/
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format="yyyy-MM-dd HH:mm:ss")@Column(name="operate_date")
+	private Date operateDate;
+	/**
+	*星级评价
+	**/
+	@ApiModelProperty(value="星级评价")
+	@Column(name="star")
+	private Integer star;
+	/**
+	*创建人id
+	**/
+	@ApiModelProperty(value="创建人id")
+	@Column(name="creater_id")
+	private String createrId;
+	/**
+	*父评论id
+	**/
+	@ApiModelProperty(value="父评论id")
+	@Column(name="c_parent_id")
+	private String cParentId;
+	/**
+	*删除状态 0正常 1删除
+	**/
+	@ApiModelProperty(value="删除状态 0正常 1删除")
+	@Column(name="is_delete")
+	private Integer isDelete;
+	/**
+	*商品id
+	**/
+	@ApiModelProperty(value="商品id")
+	@Column(name="product_id")
+	private String productId;
+	/**
+	*创建者头像
+	**/
+	@ApiModelProperty(value="创建者头像")
+	@Column(name="comment_head")
+	private String commentHead;
+	/**
+	*该评论回复的id集合，时间越早的在约前面
+	**/
+	@ApiModelProperty(value="该评论回复的id集合，时间越早的在约前面")
+	@Column(name="reply_path")
+	private String replyPath;
+	/**
+	*评论内容
+	**/
+	@ApiModelProperty(value="评论内容")
+	@Column(name="comment_content")
+	private String commentContent;
+	/**
+	*创建时间
+	**/
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format="yyyy-MM-dd HH:mm:ss")@Column(name="create_date")
+	private Date createDate;
+	
+	
+
+	
+	
+}
