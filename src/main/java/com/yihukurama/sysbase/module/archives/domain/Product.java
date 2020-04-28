5121170d62c5368d68a1b71f99ab41de3e133124
Chaos-li <179988788@qq.com>
2020-04-28 15:35:21 +0800

    商品

---
+++ src/main/java/com/yihukurama/sysbase/module/archives/domain/Product.java
@@ -0,0 +1,11 @@
+package com.yihukurama.sysbase.module.archives.domain;
+
+import com.yihukurama.sysbase.model.ProductEntity;
+
+/**
+ * @author: liyuan
+ * @date: 2020/4/28 9:54
+ * @description: 商品
+ */
+public class Product extends ProductEntity {
+}
