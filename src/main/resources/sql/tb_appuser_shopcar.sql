562590119f437d6382a5718d7619dc53d0ac4c58
yihukurama <479757405@qq.com>
2020-05-01 01:38:41 +0800

    增加购物车，订单商品模型的规格相关字段

--- src/main/resources/sql/tb_appuser_shopcar.sql
+++ src/main/resources/sql/tb_appuser_shopcar.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 01/05/2020 01:34:43
+ Date: 01/05/2020 01:38:00
 */
 
 SET NAMES utf8mb4;
--- src/main/resources/sql/tb_order_product.sql
+++ src/main/resources/sql/tb_order_product.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 01/05/2020 01:35:44
+ Date: 01/05/2020 01:38:20
 */
 
 SET NAMES utf8mb4;
