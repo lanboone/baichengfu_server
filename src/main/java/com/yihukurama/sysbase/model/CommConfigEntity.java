082e25f5893fb9f9adb20d635aac48ee9f800c8d
yihukurama <479757405@qq.com>
2020-05-05 15:37:39 +0800

    增加风格，固装和材质管理

---
+++ src/main/java/com/yihukurama/sysbase/model/CommConfigEntity.java
@@ -0,0 +1,72 @@
+package com.yihukurama.sysbase.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yihukurama.tkmybatisplus.framework.domain.tkmapper.entity.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * 说明： 业务基础配置表
+ * @author: yihukurama
+ * @date: Created in 15:18 2018/4/4
+ * @modified: by yihukurama in 15:18 2018/4/4
+ */
+@ApiModel(value="业务基础配置表")
+@Table(name="tb_comm_config")
+@Data
+public class CommConfigEntity extends BaseEntity
+{
+	/**
+	*删除状态 0正常 1删除
+	**/
+	@ApiModelProperty(value="删除状态 0正常 1删除")
+	@Column(name="is_delete")
+	private Integer isDelete;
+	/**
+	*最后修改人id
+	**/
+	@ApiModelProperty(value="最后修改人id")
+	@Column(name="operator_id")
+	private String operatorId;
+	/**
+	*最后修改日期
+	**/
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format="yyyy-MM-dd HH:mm:ss")@Column(name="operate_date")
+	private Date operateDate;
+	/**
+	*配置名称
+	**/
+	@ApiModelProperty(value="配置名称")
+	@Column(name="name")
+	private String name;
+	/**
+	*创建人id
+	**/
+	@ApiModelProperty(value="创建人id")
+	@Column(name="creater_id")
+	private String createrId;
+	/**
+	*创建时间
+	**/
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format="yyyy-MM-dd HH:mm:ss")@Column(name="create_date")
+	private Date createDate;
+	/**
+	*配置类型 10风格  20 固装  30材质
+	**/
+	@ApiModelProperty(value="配置类型 10风格  20 固装  30材质")
+	@Column(name="type")
+	private Integer type;
+	
+	
+
+	
+	
+}
