0ec6ff38ca284b19ec687a31f57d153395094a35
479757405@qq.com <479757405@qq.com>
2020-05-23 20:59:02 +0800

    订单和订单商品增加上线id 商品评论表增加商品名

--- src/main/java/com/yihukurama/sysbase/module/archives/domain/ProductComment.java
+++ src/main/java/com/yihukurama/sysbase/module/archives/domain/ProductComment.java
@@ -1,8 +1,17 @@
 package com.yihukurama.sysbase.module.archives.domain;
 
 import com.yihukurama.sysbase.model.ProductCommentEntity;
+import com.yihukurama.tkmybatisplus.app.annotation.SqlWhere;
 import lombok.Data;
 
 @Data
 public class ProductComment extends ProductCommentEntity {
+
+
+    @SqlWhere(value = SqlWhere.SqlWhereValue.LIKE,proprtityName = "product_name")
+    @Override
+    public String getProductName() {
+        return super.getProductName();
+    }
+
 }
