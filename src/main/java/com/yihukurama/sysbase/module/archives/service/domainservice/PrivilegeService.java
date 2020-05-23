edf9cbf45074ca90771b69e80b9386cb20660dce
479757405@qq.com <479757405@qq.com>
2020-05-23 17:49:26 +0800

    增加权限

--- src/main/java/com/yihukurama/sysbase/module/archives/service/domainservice/PrivilegeService.java
+++ src/main/java/com/yihukurama/sysbase/module/archives/service/domainservice/PrivilegeService.java
@@ -1,8 +1,13 @@
 package com.yihukurama.sysbase.module.archives.service.domainservice;
 
 import com.yihukurama.sysbase.model.PrivilegeEntity;
+import com.yihukurama.sysbase.model.PrivilegeEntity;
+import com.yihukurama.tkmybatisplus.app.exception.TipsException;
 import com.yihukurama.tkmybatisplus.framework.service.domainservice.CrudService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 /**
  * 说明： Privilege的领域服务
@@ -13,6 +18,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrivilegeService extends CrudService<PrivilegeEntity>{
 
+    /**
+     * 创建菜单时生成code
+     * @param data
+     * @return
+     * @throws TipsException
+     */
+    @Override
+    public PrivilegeEntity create(PrivilegeEntity data) throws TipsException {
+        data.setCode(UUID.randomUUID().toString().replaceAll("-",""));
+        return super.create(data);
+    }
 
 
 }
